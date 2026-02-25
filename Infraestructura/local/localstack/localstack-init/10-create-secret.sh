13a170c6a5a8d971fd08252366a2583a654eef46
Santiago Guerrero <ds.guerrerom1@uniandes.edu.co>
2026-02-25 00:49:21 -0500

    [BD] - despliegue BD en nube

--- Infraestructura/local/localstack/localstack-init/10-create-secret.sh
+++ Infraestructura/local/localstack/localstack-init/10-create-secret.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 
 SECRET_NAME="${DB_SECRET_NAME:-ms-franchises/mysql}"
 
-MYSQL_HOST="${MYSQL_HOST:-msfranchises-mysql}"
+MYSQL_HOST="${MYSQL_HOST:-msfranchises-dev-mysql.cgvi8qwomstp.us-east-1.rds.amazonaws.com}"
 MYSQL_PORT="${MYSQL_PORT:-3306}"
 MYSQL_DB="${MYSQL_DB:-franchises}"
 MYSQL_USER="${MYSQL_USER:-franchises_user}"
-MYSQL_PASSWORD="${MYSQL_PASSWORD:-admin}"
+MYSQL_PASSWORD="${MYSQL_PASSWORD:-JRTAuAPxxRQc4f5lW1iK2d1gw4bAEYlI}"
 
 SECRET_JSON=$(cat <<EOF
 {"host":"${MYSQL_HOST}","port":${MYSQL_PORT},"dbname":"${MYSQL_DB}","username":"${MYSQL_USER}","password":"${MYSQL_PASSWORD}"}
